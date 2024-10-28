1b7b9adb7adc82ec5c087e556b6b240be28e5436
JeromeKsiki <j.ksigzkiewiez@promethee.earth>
2024-10-28 18:01:06 +0100

    dd

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -627,34 +627,19 @@ class QSAProject:
 
 
         if symbology["type"] != "single_symbol" and symbology["type"] == "graduated" :
-            attribut = "st_length_shape"
+            attribut = "test"
             ranges = []
 
 
             symbol1 = QgsSymbol.defaultSymbol(QgsWkbTypes.LineGeometry)
             symbol1.setColor(QColor("#fecc5c"))
-            range1 = QgsRendererRange(65, 357, symbol1, "65-357")
+            range1 = QgsRendererRange(0, 4, symbol1, "65-357")
             ranges.append(range1)
 
             symbol2 = QgsSymbol.defaultSymbol(QgsWkbTypes.LineGeometry)
             symbol2.setColor(QColor("#28bceb"))
-            range2 = QgsRendererRange(357, 630, symbol2, "357-630")
+            range2 = QgsRendererRange(4, 8, symbol2, "357-630")
             ranges.append(range2)
-
-            symbol3 = QgsSymbol.defaultSymbol(QgsWkbTypes.LineGeometry)
-            symbol3.setColor(QColor("#a4fc3c"))
-            range3 = QgsRendererRange(630, 1057, symbol3, "630-1057")
-            ranges.append(range3)
-
-            symbol4 = QgsSymbol.defaultSymbol(QgsWkbTypes.LineGeometry)
-            symbol4.setColor(QColor("#fb7e21"))
-            range4 = QgsRendererRange(1057, 1791, symbol4, "1057-1791")
-            ranges.append(range4)
-
-            symbol5 = QgsSymbol.defaultSymbol(QgsWkbTypes.LineGeometry)
-            symbol5.setColor(QColor("#7a0403"))
-            range5 = QgsRendererRange(1791, 11039, symbol5, "1791-11039")
-            ranges.append(range5)
 
             render = QgsGraduatedSymbolRenderer(attribut, ranges)
             render.setMode(QgsGraduatedSymbolRenderer.Custom) 
