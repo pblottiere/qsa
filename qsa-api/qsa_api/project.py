a65bf0405a4d2a9daa3d18aa714400c401ec2956
JeromeKsiki <j.ksigzkiewiez@promethee.earth>
2024-11-21 16:39:21 +0100

    reduce mapuniscale

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -766,7 +766,7 @@ class QSAProject:
                 drop_shadow.setOffsetAngle(155)
                 drop_shadow.setOffsetDistance(1.2)
                 drop_shadow.setOffsetUnit(QgsUnitTypes.RenderPixels)
-                drop_shadow.setOffsetMapUnitScale(QgsMapUnitScale(0, 100000))
+                drop_shadow.setOffsetMapUnitScale(QgsMapUnitScale(0, 2.5))
 
                 effect_stack.appendEffect(drop_shadow)
 
