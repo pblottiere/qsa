a567704c56af89ffdcb0da60696a5426a59ef3e7
JeromeKsiki <j.ksigzkiewiez@promethee.earth>
2024-11-21 18:50:31 +0100

    Millimeters

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -761,7 +761,7 @@ class QSAProject:
                 drop_shadow = QgsDropShadowEffect()
 
                 drop_shadow.setColor(QColor(23, 23, 23, 200))  
-                drop_shadow.setOffsetUnit(QgsUnitTypes.RenderPixels)
+                drop_shadow.setOffsetUnit(QgsUnitTypes.RenderMillimeters)
                 drop_shadow.setBlurUnit(QgsUnitTypes.RenderPixels)
                 drop_shadow.setBlurLevel(2)
                 drop_shadow.setOffsetAngle(155)
