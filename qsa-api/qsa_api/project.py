ca270c7ab9f7d5a94d1ce4ef1f4356f077d37936
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2024-12-12 16:48:54 +0100

    ccc

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -253,7 +253,7 @@ class QSAProject:
 
         if style_name != "default" and style_name not in self.styles:
             return False, f"Style '{style_name}' does not exist"
-        thread = threading.Thread(target=threaded_clear_cache, args=(self.name, layer_name))
+        thread = threading.Thread(target=self.threaded_clear_cache, args=(self.name, layer_name))
         thread.start()
 
         flags = Qgis.ProjectReadFlags()
