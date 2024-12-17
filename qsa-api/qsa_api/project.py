6a0bddb88ad23377b6fa058766abb3bdcfc79e3f
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2024-12-17 14:11:33 +0100

    f

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -279,7 +279,6 @@ class QSAProject:
         flags |= Qgis.ProjectReadFlag.FlagDontLoadLayouts
         flags |= Qgis.ProjectReadFlag.FlagTrustLayerMetadata
         flags |= Qgis.ProjectReadFlag.FlagDontLoad3DViews
-        flags |= Qgis.ProjectReadFlag.FlagDontStoreOriginalStyles
 
         project = QgsProject()
         project.read(self._qgis_project_uri, flags)
