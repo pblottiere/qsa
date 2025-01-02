df27443f7b73251e44cf487c75fb66b8a1be4d3e
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2025-01-02 18:05:42 +0100

    categorized_value["value"]

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -717,7 +717,7 @@ class QSAProject:
                     svg_layer.setSize(categorized_value["size"])
                     symbol.changeSymbolLayer(0, svg_layer)
                     
-                    range = QgsRendererRange(categorized_value["min"], categorized_value["max"], symbol, "test")
+                    range = QgsRendererRange(categorized_value["value"], symbol, "test")
                     ranges.append(range)
             case other:  
                     return None #Not implement
