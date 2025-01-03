57770793bb179ea0ab78a20336931f9994b49168
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2025-01-03 15:03:47 +0100

    litéral

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -347,13 +347,13 @@ class QSAProject:
             self.debug("test exist 2")
             service = config().qgisserver_projects_psql_service
             self.debug(f"test exist 4  : service {service}")
-            uri = f"postgresql:?service={service}&schema={self.schema}"
+            uri = f"postgresql://qsa:qsa@localhost:5432/qsa?sslmode=disable&schema=public"
             self.debug(f"test exist 5 service : {service} - &schema={self.schema}")
 
             storage = (
                 QgsApplication.instance()
                 .projectStorageRegistry()
-                .projectStorageFromType("postgres")
+                .projectStorageFromType("postgresql")
             )
             self.debug("test exist 6")
             projects = storage.listProjects(uri)
