9a31b2ebf2c2389f3b98443c82f6041111b8a473
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2025-01-06 11:38:17 +0100

    //

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -911,7 +911,7 @@ class QSAProject:
             password = config().qgisserver_projects_psql_password
             host = config().qgisserver_projects_psql_host
             port = config().qgisserver_projects_psql_port
-            self.debug(f"postgresql:?qsa:qsa@postgres:5432/qsa?sslmode=disable&schema=public&project={self.name}")
-            return f"postgresql:?qsa:qsa@postgres:5432/qsa?sslmode=disable&schema=public&project={self.name}"
+            self.debug(f"postgresql://?qsa:qsa@postgres:5432/qsa?sslmode=disable&schema=public&project={self.name}")
+            return f"postgresql://?qsa:qsa@postgres:5432/qsa?sslmode=disable&schema=public&project={self.name}"
         else:
             return (self._qgis_project_dir / f"{self.name}.qgs").as_posix()
