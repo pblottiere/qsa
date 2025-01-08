14d1aa2be6affa3d74acae37a0499f51e669c987
Jérôme Ksigzkiewiez <j.ksigzkiewiez@promethee.earth>
2025-01-08 09:27:30 +0100

    setStrokeColor

--- qsa-api/qsa_api/project.py
+++ qsa-api/qsa_api/project.py
@@ -815,7 +815,7 @@ class QSAProject:
                     svg_layer = QgsSvgMarkerSymbolLayer(properties["symbol_path"])
                     
                     svg_layer.setColor(QColor(int(testSplit[0]),int(testSplit[1]),int(testSplit[2]),int(testSplit[3])))
-                    svg_layer.strokeColor(QColor(0,0,0,int(testSplit[3])))
+                    svg_layer.setStrokeColor(QColor(0,0,0,int(testSplit[3])))
                     svg_layer.setSize(properties["size"])
                     symbol.changeSymbolLayer(0, svg_layer)
             case other:  
